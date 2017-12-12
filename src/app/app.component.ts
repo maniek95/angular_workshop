3169b13bcdc5047fd2a2b23b9b1d6e0519d8c03d
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2017-12-12 16:02:17 +0100

    component lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,55 @@
 import { Component } from '@angular/core';
+import { OnInit, OnDestroy, OnChanges, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy, OnChanges, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
+
   title: string = 'app';
   showContent: boolean = false;
 
   welcomeList: string[] = [];
 
   constructor() {
+    this.print('constructor');
     for (let i = 0; i < 10; i++) {
       this.welcomeList.push(`Witaj na code europe ${i + 1} gościu`);
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.print('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    this.print('ngAfterViewChecked');
+  }
+
+  ngAfterContentInit(): void {
+    this.print('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    this.print('ngAfterContentChecked');
+  }
+
+  ngOnChanges(changes: any): void {
+    this.print('ngOnChanges');
+  }
+
+  ngOnDestroy(): void {
+    this.print('ngOnDestroy');
+  }
+
+  ngOnInit(): void {
+    this.print('ngOnInit');
+  }
+
+  private print(from: string) {
+    console.log(from);
   }
 
 }
