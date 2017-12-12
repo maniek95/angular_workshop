39deb34e3d4e3c06a04e4e9d97bea63463e6cf3f
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2017-12-12 15:57:17 +0100

    html directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app';
+  showContent: boolean = false;
+
+  welcomeList: string[] = [];
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.welcomeList.push(`Witaj na code europe ${i + 1} gościu`);
+    }
+  }
+
 }
