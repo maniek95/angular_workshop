b1f2ca1bdea87cf28991985d0ae9dac667a0770a
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2017-12-12 20:33:06 +0100

    added publications

---
+++ src/app/publications/publications.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PublicationsRoutingModule } from './publications-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PublicationsRoutingModule
+  ],
+  declarations: []
+})
+export class PublicationsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
-    CommonModule
+    CommonModule,
+    HttpModule
   ],
   declarations: []
 })
