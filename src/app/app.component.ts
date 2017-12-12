0a621cb9e3d833d69ddeb90f4a54df0230d4cc47
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2017-12-12 11:31:54 +0100

    added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,48 +3,8 @@ import { Component, OnChanges, AfterViewInit, AfterViewChecked, AfterContentChec
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnChanges, AfterViewInit, AfterViewChecked, AfterContentChecked, AfterContentInit, OnDestroy, OnInit {
-  title: string = 'app';
-  showContent: boolean = false;
-
-  welcomeList: string[] = [];
-
-  constructor() {
-    console.log('constructor');
-
-    for (let i = 0; i < 10; i++) {
-      this.welcomeList.push(`Witamy na codeeurope poraz ${i + 1}`);
-    }
-  }
-
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-  }
-
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
-  }
-
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-  }
-
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked');
-  }
-
-  ngOnChanges(changes: any): void {
-    console.log('ngOnChanges');
-  }
-
-  ngOnInit(): void {
-    console.log('ngOnInit');
-  }
-
-  ngOnDestroy(): void {
-    console.log('ngOnDestroy');
-  }
+export class AppComponent{
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule
   ],
