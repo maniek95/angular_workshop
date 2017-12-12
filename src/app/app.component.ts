7a21f21bb945f315ec23d45ed28d31063643e160
Gracjan Górecki <gorecki.gracjan@hotmail.com>
2017-12-12 11:04:16 +0100

    html directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showContent: boolean = false;
+
+  welcomeList: string[] = [];
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.welcomeList.push(`Witamy na codeeurope poraz ${i + 1}`);
+    }
+  }
 }
